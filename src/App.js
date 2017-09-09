0deabdf1d00728264665cfe5e595c5de53ec90e2
Will Howlett <Will_Howlett@dennis.co.uk>
2017-09-09 12:21:48 +0100

    Basic board layout config (unlikely to be final obvs)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import boardLayout from './config/boardlayout.json';
 import Board from './components/Board';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log(boardLayout);
     return (
       <div className="board">
         <header className="board-header">
